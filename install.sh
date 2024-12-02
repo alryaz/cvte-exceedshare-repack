42a7f88488251eb0a11bd8191bd08103222ff182
Alexander <alryaz@alryaz.com>
2024-12-02 13:53:56 +0300

    refactor: changed repository name

--- install.sh
+++ install.sh
@@ -5,10 +5,10 @@ if [ "$(whoami)" != "root" ]; then
     SUDO=sudo
 fi
 
-KEYRING=/usr/share/keyrings/cvte.exceedshare.debian.gpg
+KEYRING=/usr/share/keyrings/cvte.exceedshare.repack.gpg
 
 ${SUDO} apt-get update
 ${SUDO} apt-get -y install apt-transport-https
-wget -qO- https://alryaz.github.io/cvte-exceedshare-debian/public.key | gpg --dearmor - | ${SUDO} tee $KEYRING >/dev/null
-${SUDO} sh -c 'echo "deb [signed-by='$KEYRING'] https://alryaz.github.io/cvte-exceedshare-debian/repo/ debian main" > /etc/apt/sources.list.d/cvte.exceedshare.list'
+wget -qO- https://alryaz.github.io/cvte-exceedshare-repack/public.key | gpg --dearmor - | ${SUDO} tee $KEYRING >/dev/null
+${SUDO} sh -c 'echo "deb [signed-by='$KEYRING'] https://alryaz.github.io/cvte-exceedshare-repack/repo/ debian main" > /etc/apt/sources.list.d/cvte.exceedshare.list'
 ${SUDO} apt-get update
